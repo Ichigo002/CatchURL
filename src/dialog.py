ef0945e4b57681db433b3e11cd40351a0c9a96d1
Ichigo002 <87382616+Ichigo002@users.noreply.github.com>
2023-10-01 08:32:26 +0200

    try fresh start

---
+++ src/dialog.py
@@ -0,0 +1,17 @@
+#This is dialog script
+# It's responsible for chattig with user and dispalyig info at cmd. Huyututyutuy
+
+class Dialog:
+    app_name = "CatchURL"
+
+    def __init__(self) -> None:
+        print(f'Welcome in {self.app_name}')
+
+    def startDialog(self):
+        print("Loading CSV.")
+        #os.system("cls")
+        print("Loading CSV..")
+        #os.system("cls")
+        print("Loading CSV...")
+        #os.system("cls")
+
---
+++ src/main.py
@@ -0,0 +1,14 @@
+# Hello, This program can me your life easier cuz:
+# 1. It makes possible to download your favourite hentai anime faster;
+# 2. It just is easy to use;
+# 3. It's first porjct which I start. For a long time I was burnt out by school
+import dialog as dl
+
+
+def main():
+    d = dl.Dialog()
+    d.startDialog()
+
+
+if __name__ == "__main__":
+    main()
