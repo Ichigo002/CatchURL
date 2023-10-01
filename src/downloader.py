9049dba4f9ed6ffcaca8ae4ee6ce61f886790528
Ichigo002 <87382616+Ichigo002@users.noreply.github.com>
2023-10-01 12:08:46 +0200

    create downloader

---
+++ src/downloader.py
@@ -0,0 +1,10 @@
+# this is downloader class.
+# Created by Wiktor Bojanowksi. eee lu wo ma. Kaichou wa maid sama!
+
+class Downloader():
+    
+    def __init__(self) -> None:
+        print("Hello, im downloader")
+
+
+    def 
--- src/main.py
+++ src/main.py
@@ -1,15 +1,17 @@
 # Attempt 2 in this pice of shit
 import csvmanager as csv
+import downloader as dwn
 
 def main():
     csvm = csv.CSVManager("data/urls.csv")
-
+    dwnl = dwn.Downloader()
+"""
     for i in range(15):
         if not csvm.is_end_data():
             print(csvm.fetch_url())
             print(csvm.fetch_filename())
             csvm.next()
-
+"""
 
 
 
